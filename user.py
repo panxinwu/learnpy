3758495735ada5976e0304853be87ec7b36bfbe5
xinwu.pxw <xinwu.pxw@alibaba-inc.com>
2019-01-03 02:23:54 +0800

    first commit

---
+++ user.py
@@ -0,0 +1,9 @@
+account = 'aaa'
+password = 'bbb'
+print('please input username')
+username = input()
+print('please input pass')
+user_password = input()
+
+if (username == account) and (password == user_password) :
+  print('login success')
